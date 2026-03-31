0ccd7378ed2865268fdc011eecc0f9bac2d2305d
dardenkyle <darden_kyle@hotmail.com>
2026-03-31 17:29:56 -0500

    Remove DemoScraper from the scraping module exports (unused/ to be added later)

--- cs2_analytics/scrapers/__init__.py
+++ cs2_analytics/scrapers/__init__.py
@@ -3,6 +3,5 @@
 from .results_scraper import ResultsScraper
 from .match_scraper import MatchScraper
 from .map_scraper import MapScraper
-from .demo_scraper import DemoScraper
 
-__all__ = ["ResultsScraper", "MatchScraper", "MapScraper", "DemoScraper"]
+__all__ = ["ResultsScraper", "MatchScraper", "MapScraper"]
